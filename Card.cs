1f60befffc311ac87012e265ee787e9a7f94b373
Logan Butler <logan3528@gmail.com>
2022-05-10 11:17:32 -0600

    First Commit

---
+++ Card.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Prove02
+{
+    public class Card
+    {
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prove02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dealer dealer = new Dealer();
+        }
+    }
+}
